f41648cbf0d77032a58cfb882e8d867588e15a42
Ashutosh Sharma <asharma89505@gmail.com>
2024-03-31 01:31:04 +0530

    update api error handler

--- server/utils/ApiError.js
+++ server/utils/ApiError.js
@@ -1,11 +1,27 @@
 class ApiError extends Error {
-  constructor(statusCode, message, errors=[]) {
-    super();
-    this.statusCode = statusCode;
-    this.message = message;
-    this.success = false;
-    this.errors = errors;
-  }
-}
+    constructor(
+        statusCode,
+        message= "Something went wrong",
+        errors = [],
+        stack = "",
+        data
+    ){
+        super()
+        this.statusCode = statusCode
+        this.data = data
+        this.message = message
+        this.success = false;
+        this.errors = errors
 
+        if (stack) {
+            this.stack = stack
+        } else{
+            Error.captureStackTrace(this, this.constructor)
+        }
+
+    }
+}
+// const errorHandler = (statusCode, message, data) => {
+//   return new ErrorHandler(statusCode, message, data);
+// };
 module.exports = ApiError;
