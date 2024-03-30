511683b99df0602f12bc5e49af6e787ecc2f0bea
Ashutosh Sharma <asharma89505@gmail.com>
2024-03-31 01:37:29 +0530

    request-ip module configured

--- server/controllers/weather.controller.js
+++ server/controllers/weather.controller.js
@@ -1,13 +1,19 @@
 const ApiError = require("../utils/ApiError");
 const APiResponse = require("../utils/ApiResponse");
+var requestIp = require("request-ip");
 
 const getWeatherByIp = async (req, res) => {
   try {
-    const ip = req.ip;
+    const ip = requestIp.getClientIp(req);
+
     console.log(ip);
-    res.status(200).json(new APiResponse(200, "Your Ip Address", { ip }));
+    res
+      .status(200)
+      .json(new APiResponse(200, "Weather Fetch Successfully", { ip }));
   } catch (error) {
-    res.status(500).json(new ApiError(500, error.message || "Internal Server Error"));
+    res
+      .status(500)
+      .json(new ApiError(500, error.message || "Internal Server Error"));
   }
 };
 
