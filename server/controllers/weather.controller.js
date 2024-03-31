22933347cbaddde4baa4b6af96be6baf5ad98d1e
Ashutosh Sharma <asharma89505@gmail.com>
2024-03-31 18:49:46 +0530

    fix getWeatherByIp

--- server/controllers/weather.controller.js
+++ server/controllers/weather.controller.js
@@ -1,3 +1,4 @@
+const { weatherByIp } = require("../api/api");
 const ApiError = require("../utils/ApiError");
 const APiResponse = require("../utils/ApiResponse");
 var requestIp = require("request-ip");
@@ -5,11 +6,29 @@ var requestIp = require("request-ip");
 const getWeatherByIp = async (req, res) => {
   try {
     const ip = requestIp.getClientIp(req);
-
-    console.log(ip);
+    const response = await weatherByIp(ip);
+    if(!response) throw new ApiError(500, "Internal Server Error");
+    const data = {
+      location:{
+        name:response.location.name,
+        region:response.location.region,
+        country:response.location.country,
+        localtime:response.location.localtime
+      },
+      current:{
+        temp_c:response.current.temp_c,
+        humidity:response.current.humidity,
+        cloud:response.current.cloud,
+        wind_kph:response.current.wind_kph,
+        condition:{
+          text:response.current.condition.text,
+          icon:response.current.condition.icon
+        }
+      }
+    }
     res
       .status(200)
-      .json(new APiResponse(200, "Weather Fetch Successfully", { ip }));
+      .json(new APiResponse(200, "Weather Fetch Successfully",  data ));
   } catch (error) {
     res
       .status(500)
